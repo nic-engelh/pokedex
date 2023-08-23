6a16793c249ad52296c563e6c46e99134959ed18
nic-engelh <NiklasEngelhardt@gmail.com>
2023-08-23 11:30:54 +0200

    Changed readability of read und transfer data functions. Added get-functions for pokemon types and sprites

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const POKEMONNAMES = new Set(POKEMON);
 // stores key (pokemonName) and value (Pokemon JSON)
 let pokemonCache = new Map();
 
-let readPokemonCount = 0; 
+let currentLoadedPokemon = 0; 
 
 // Next steps:
 // load 12 pokemon into pokemonCache
@@ -12,7 +12,10 @@ let readPokemonCount = 0;
 // readPokemonFromList returns 12 PokemonNames
 // those names are used to fetch the corresponding JSON via API
 
+// renderCards - a function which renders initial cards from pokemonCache according to the current Pokemon count
+// renderCurrentPokemonDetailCard - renders detail card of the clicked pokemons abilities
 
+// getPokemonTyp 
 
 function init() {
 
@@ -22,42 +25,33 @@ function init() {
 
 }
 
-function transferPokemonIntoCache  (number) {
-    
-    readPokemonCount = readPokemonCount + number;
-    
-    for (let index = 0; index <= readPokemonCount; index++) {
-        const element = array[index];
-
-        
-    }
-
+async function transferPokemonIntoCache  (pokemon) {
+    let pokemonData = await getPokemonData(pokemon);
+    pokemonCache.set(pokemon, pokemonData);
+    return true;
 }
 
-async function readPokemonFromList(count) {
+function readPokemonFromList(count) {
+    currentLoadedPokemon = currentLoadedPokemon + count;
     let index = 0;
     for (const pokemon of POKEMONNAMES) {
-        let pokemonDataAsJSON = await getPokemonData(pokemon);
-        pokemonCache.set(pokemon, pokemonDataAsJSON);
+        if (index > currentLoadedPokemon){
+            break;
+        } else if (pokemonCache.has(pokemon)){
+            continue;
+        }
+        transferPokemonIntoCache(pokemon);
         index++;
-        if (index > count){
-        return false
-        };
     };
+    return true;
 }
-
 
 async function getPokemonData(pokemon) {
 
     pokemon = pokemon.toLowerCase();
-
     let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-
     let pokemonDataAsText = await fetch(url);
-
     let pokemonDataAsJSON = await pokemonDataAsText.json();
-
     console.log(pokemonDataAsJSON);
-
     return pokemonDataAsJSON;
 }
